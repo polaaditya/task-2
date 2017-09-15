06660f6a19f383d28c3fa953a6a113fe78fbd499
polaaditya <polaaditya@gmail.com>
2017-09-15 14:39:29 -0400

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,14 @@ directory "#{node['iis']['docroot']}/icons" do
   action :create
 end
 
+# Configuring Icons
+template "#{node['iis']['docroot']}/icons/index.html" do
+  source 'icons.html.erb'
+  mode 0664
+  sensitive true
+  only_if { File.exist?("#{node['iis']['docroot']}/icons/index.html") }
+end
+
 # now create and start the site (note this will use the default application pool which must exist)
 iis_site 'Icon Site' do
   protocol :http
